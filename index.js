8c96bdb342452760dfddbd3a68f7df126a19c2e8
Petko D. Petkov <pdp@aluminum.home>
2014-01-10 23:01:01 +0000

    Modified for better browserify support.

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var DomUtils = module.exports;
 
+// ---
+
+var stringify = require('./lib/stringify');
+var traversal = require('./lib/traversal');
+var manipulation = require('./lib/manipulation');
+var querying = require('./lib/querying');
+var legacy = require('./lib/legacy');
+var helpers = require('./lib/helpers');
+
+// ---
+
 [
-	"stringify", "traversal", "manipulation", "querying", "legacy", "helpers"
-].forEach(function(name){
-	var ext = require("./lib/" + name);
+	stringify, traversal, manipulation, querying, legacy, helpers
+].forEach(function(ext){
 	Object.keys(ext).forEach(function(key){
 		DomUtils[key] = ext[key].bind(DomUtils);
 	});
